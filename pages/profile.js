b08d18afb75dbff55504381056fda12d5bb25800
andasan <andasan@gmail.com>
2021-06-25 18:31:56 -0700

    added sign in functionality

--- pages/profile.js
+++ pages/profile.js
@@ -2,23 +2,55 @@ import { useState, useEffect } from 'react'
 import { Auth } from 'aws-amplify'
 import '../configureAmplify'
 
+import SignIn from '../components/SignIn'
+
+const initialState = { email: '', password: '', authCode: '' }
+
 const Profile = () => {
+  const [uiState, setUiState] = useState(null)
+  const [formstate, setFormState] = useState(initialState)
+  const [user, setUser] = useState(null)
+
   useEffect(() => {
-      const checkUser = async () => {
+    const checkUser = async () => {
+      try {
         const user = await Auth.currentAuthenticatedUser()
-        console.log({ user });
+        setUser(user)
+        setUiState('signedIn')
+      } catch (error) {
+        setUser(null)
+        setUiState('signIn')
+        console.error(error.message)
       }
-      checkUser()
+    }
+    checkUser()
   }, [])
+
+  const onChange = (e) => {
+    setFormState({ ...formstate, [e.target.name]: e.target.value })
+  }
+
   return (
-    <div>
-      <button onClick={() => Auth.federatedSignIn({ provider: 'Google' })}>
-        Sign in with Google
-      </button>
-      <button onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })}>
-        Sign in with Facebook
-      </button>
-    </div>
+    <>
+      {uiState === 'signIn' && (
+        <SignIn onChange={onChange} setUiState={setUiState} />
+      )}
+      {uiState === 'signedIn' && (
+        <div>
+          <p className="text-x1">Welcome, {user.attributes.email}</p>
+          <button
+            className="text-white w-full mt-10 bg-pink-600 p-3 rounded"
+            onClick={() => {
+              Auth.signOut()
+              setUiState('signIn')
+              setUser(null)
+            }}
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
+    </>
   )
 }
 
